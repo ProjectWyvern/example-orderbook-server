e43d0f8e1b4cfe1ab5bbcaedb3ea71f2a5995a19
Protinam <protinam@protonmail.ch>
2018-01-17 20:49:38 -0800

    Basic server

--- server/db.js
+++ server/db.js
@@ -12,9 +12,71 @@ const sequelize = new Sequelize('wyvernOrderbook', null, null, {
 })
 
 const Order = sequelize.define('order', {
+  hash: {type: Sequelize.TEXT, allowNull: false, unique: true},
+  metadata: {type: Sequelize.JSON, allowNull: false},
+  exchange: {type: Sequelize.TEXT, allowNull: false},
+  initiator: {type: Sequelize.TEXT, allowNull: false},
+  side: {type: Sequelize.INTEGER.UNSIGNED, allowNull: false},
+  saleKind: {type: Sequelize.INTEGER.UNSIGNED, allowNull: false},
+  target: {type: Sequelize.TEXT, allowNull: false},
+  howToCall: {type: Sequelize.INTEGER.UNSIGNED, allowNull: false},
+  calldata: {type: Sequelize.TEXT, allowNull: false},
+  replacementPattern: {type: Sequelize.TEXT, allowNull: false},
+  metadataHash: {type: Sequelize.TEXT, allowNull: false},
+  paymentToken: {type: Sequelize.TEXT, allowNull: false},
+  basePrice: {type: Sequelize.TEXT, allowNull: false},
+  extra: {type: Sequelize.TEXT, allowNull: false},
+  listingTime: {type: Sequelize.TEXT, allowNull: false},
+  expirationTime: {type: Sequelize.TEXT, allowNull: false},
+  frontend: {type: Sequelize.TEXT, allowNull: false},
+  salt: {type: Sequelize.TEXT, allowNull: false}
+})
+
+const encodeOrder = (order) => ({
+  hash: order.hash,
+  metadata: order.metadata,
+  exchange: order.exchange,
+  initiator: order.initiator,
+  side: order.side,
+  saleKind: order.saleKind,
+  target: order.target,
+  howToCall: order.howToCall,
+  calldata: order.calldata,
+  replacementPattern: order.replacementPattern,
+  metadataHash: order.metadataHash,
+  paymentToken: order.paymentToken,
+  basePrice: JSON.stringify(order.basePrice),
+  extra: JSON.stringify(order.extra),
+  listingTime: JSON.stringify(order.listingTime),
+  expirationTime: JSON.stringify(order.expirationTime),
+  frontend: order.frontend,
+  salt: JSON.stringify(order.salt)
+})
+
+const decodeOrder = (order) => ({
+  hash: order.hash,
+  metadata: order.metadata,
+  exchange: order.exchange,
+  initiator: order.initiator,
+  side: order.side,
+  saleKind: order.saleKind,
+  target: order.target,
+  howToCall: order.howToCall,
+  calldata: order.calldata,
+  replacementPattern: order.replacementPattern,
+  metadataHash: order.metadataHash,
+  paymentToken: order.paymentToken,
+  basePrice: JSON.parse(order.basePrice),
+  extra: JSON.parse(order.extra),
+  listingTime: JSON.parse(order.listingTime),
+  expirationTime: JSON.parse(order.expirationTime),
+  frontend: order.frontend,
+  salt: JSON.parse(order.salt)
 })
 
 module.exports = {
   sequelize: sequelize,
-  Order: Order
+  Order: Order,
+  encodeOrder: encodeOrder,
+  decodeOrder: decodeOrder
 }
--- server/index.js
+++ server/index.js
@@ -1,23 +1,53 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+// const helmet = require('helmet')
 
-const { sequelize } = require('./db.js')
+const { sequelize, Order, encodeOrder, decodeOrder } = require('./db.js')
 const log = require('./logging.js')
 
 const app = express()
+// app.use(helmet())
 app.use(bodyParser.json())
 app.use((req, res, next) => {
   log.debug({path: req.path, method: req.method, ip: req.headers['X-Forwarded-For']}, 'Request received')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
+
+const router = express.Router()
+
+router.get('/orders', (req, res) => {
+  Order.findAll().then(orders => {
+    res.json(orders.map(decodeOrder))
+  })
+})
+
+router.get('/orders/:hash', (req, res) => {
+  Order.findOne({hash: req.params.hash}).then(order => {
+    res.json(decodeOrder(order))
+  })
+})
+
+router.post('/orders/post', (req, res) => {
+  const order = req.body
+  Order.create(encodeOrder(order)).then(() => {
+    res.json({result: true, errors: []})
+  })
+})
+
+router.post('/orders/validate', (req, res) => {
+  res.json({result: true, errors: []})
+})
+
+app.use('/v1', router)
 
 const go = ({ port }) => {
   sequelize
     .sync()
     .then(() => {
       app.listen(port, () => {
-        log.debug({port: port}, 'Server started')
+        log.debug({origin: 'express', port: port}, 'Server started')
       })
     })
 }
