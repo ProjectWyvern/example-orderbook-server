124851951f4353aa721c94bf6fd55864210eea53
Protinam <protinam@protonmail.ch>
2018-02-14 17:04:32 -0800

    Minor Varnish fixes

--- proxy/varnish/restart.sh
+++ proxy/varnish/restart.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-varnishadm -T localhost:6081 -S /tmp/secret vcl.load reload /etc/varnish/default.vcl
-varnishadm -T localhost:6081 -S /tmp/secret vcl.use reload
+varnishadm -T localhost:6081 -S /tmp/secret vcl.load one /etc/varnish/default.vcl
+varnishadm -T localhost:6081 -S /tmp/secret vcl.use one
+varnishadm -T localhost:6081 -S /tmp/secret vcl.discard two
+varnishadm -T localhost:6081 -S /tmp/secret vcl.load two /etc/varnish/default.vcl
+varnishadm -T localhost:6081 -S /tmp/secret vcl.use two
+varnishadm -T localhost:6081 -S /tmp/secret vcl.discard one
 
 exit 0
